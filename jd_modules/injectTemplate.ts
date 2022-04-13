b66fd5317e539dcfcba9ed607fc493fedc9339d9
JustinDudley <justindudley99@gmail.com>
2022-04-13 14:47:19 -0400

    add comment

--- jd_modules/injectTemplate.ts
+++ jd_modules/injectTemplate.ts
@@ -1,4 +1,4 @@
-export {}; // typescript quick-fix
+export {}; // typescript quick-fix, turns this file into a ts module, I believe
 
 const injectTemplate = (template: String, name: String) =>
     template.replace(/%LANGUAGE%/g, `${name}`);
