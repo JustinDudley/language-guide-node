ce4c3c712db257b607c4c6699e8a3f86431bc862
JustinDudley <justindudley99@gmail.com>
2022-04-13 11:33:07 -0400

    remove console.log

--- index.ts
+++ index.ts
@@ -12,8 +12,6 @@ const server = http.createServer(
         res.end(
             '<html><head><link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/313/chequered-flag_1f3c1.png" /></head><body><div>Hello World!</div></body></html>',
         );
-
-        console.log(req.url);
     },
 );
 
